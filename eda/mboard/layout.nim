06f9f3dbf23e2bf32f5b0d1ccc66573dbe331787
Kier Davis <kierdavis@gmail.com>
2016-05-11 15:19:32 +0100

    Implement placement of DIP sockets and breakout pins in layout script

--- eda/mboard/layout.nim
+++ eda/mboard/layout.nim
@@ -1,3 +1,4 @@
+import pcb.manip
 import pcb.parse
 import pcb.stringify
 import pcb.types
@@ -8,7 +9,53 @@ var pcb = parseStdin()
 pcb.name = "mboard"
 
 # Set board dimensions
-pcb.width = 10.cm()
-pcb.height = 10.cm()
+const width = 10.cm()
+const height = 10.cm()
+pcb.width = width
+pcb.height = height
+
+# Move DIP16 items into position
+const midX = width / 2
+const midY = height / 2
+const dipWidth = 700.mil()
+const dipHeight = 300.mil()
+const dipHozSeparation = 200.mil()
+const dipVertSeparation = 600.mil()
+const dipAnchorsX = [
+  midX - dipHozSeparation/2 - dipWidth - dipHozSeparation - dipWidth,
+  midX - dipHozSeparation/2 - dipWidth,
+  midX + dipHozSeparation/2,
+  midX + dipHozSeparation/2 + dipWidth + dipHozSeparation,
+]
+const dipAnchorsY = [
+  midY - dipVertSeparation/2 - dipHeight - dipVertSeparation,
+  midY - dipVertSeparation/2,
+  midY + dipVertSeparation/2 + dipHeight,
+  midY + dipVertSeparation/2 + dipHeight + dipVertSeparation + dipHeight,
+]
+for i in 0 .. 15:
+  let elementName = "U" & $(i + 1) & "/CONN3"
+  pcb.mfindElement(elementName).rotateCCW()
+  pcb.mfindElement(elementName).markPos = (
+    x: dipAnchorsX[i mod 4],
+    y: dipAnchorsY[i div 4],
+  )
+
+# Move upper breakout pins into position
+for i in 0 .. 15:
+  let elementName = "U" & $(i + 1) & "/CONN1"
+  pcb.mfindElement(elementName).markPos = (
+    x: dipAnchorsX[i mod 4] + dipWidth/2,
+    y: dipAnchorsY[i div 4] - dipHeight - 150.mil(),
+  )
+
+# Move lower breakout pins into position
+for i in 0 .. 15:
+  let elementName = "U" & $(i + 1) & "/CONN2"
+  pcb.mfindElement(elementName).rotate180()
+  pcb.mfindElement(elementName).markPos = (
+    x: dipAnchorsX[i mod 4] + dipWidth/2,
+    y: dipAnchorsY[i div 4] + 150.mil(),
+  )
 
 echo $pcb
