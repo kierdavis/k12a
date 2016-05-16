ab1132e264907de88d8defa13af25cfef7bcb9b9
Kier Davis <kierdavis@gmail.com>
2016-05-16 21:07:42 +0100

    Switch from using hacked-together footprintdsl to the same nimpcb library as used for PCB layout

--- eda/footprints/BREAKOUT_PADS.nim
+++ eda/footprints/BREAKOUT_PADS.nim
@@ -1,43 +1,50 @@
-import k12a.eda.footprintdsl
+import pcb.types
+import pcb.stringify
 
-var element = Element(
-  flags: {},
-  description: "Two rows of 8 breakout pads",
-  markPosition: fromMils(400, 100),
-  namePosition: fromMils(-350, -160),
-  nameRotation: 0,
-  nameScale: 100,
-  nameFlags: {},
-  children: @[],
-)
+proc createElement(): Element =
+  const pinDrillDiameter = 28.mil()
+  const pinMetalDiameter = pinDrillDiameter + 2 * 16.mil()
+  const pinMaskDiameter = pinMetalDiameter + 2 * 3.mil()
+  #const silkThickness = 10.mil()
+  
+  var element: Element
+  element.init()
+  element.desc = "BREAKOUT_PADS"
+  element.markPos = (x: 400.mil(), y: 100.mil())
+  element.textPos = (x: -350.mil(), y: -160.mil())
+  element.textDir = leftToRight
+  element.textScale = 100
+  
+  result = element
+  
+  for i in 0..7:
+    let x = (i*2 - 7) * 50.mil()
+    let number = $(i + 1)
+    let name = "pin" & number
+    
+    var upperPin: Pin
+    upperPin.init()
+    upperPin.center = (x: x, y: 50.mil())
+    upperPin.metalDiameter = pinMetalDiameter
+    upperPin.clearance = 30.mil()
+    upperPin.maskDiameter = pinMaskDiameter
+    upperPin.drillDiameter = pinDrillDiameter
+    upperPin.name = name
+    upperPin.number = number
+    element.pins.add(upperPin)
+    
+    var lowerPin: Pin
+    lowerPin.init()
+    lowerPin.center = (x: x, y: -50.mil())
+    lowerPin.metalDiameter = pinMetalDiameter
+    lowerPin.clearance = 30.mil()
+    lowerPin.maskDiameter = pinMaskDiameter
+    lowerPin.drillDiameter = pinDrillDiameter
+    lowerPin.name = name
+    lowerPin.number = number
+    element.pins.add(lowerPin)
+  
+  result = element
 
-let pinDrillDiameter = fromMils(28) # 22AWG hook-up wire is just over 0.6mm diameter
-let pinMetalDiameter = pinDrillDiameter + 2*fromMils(16)
-let pinMaskDiameter = pinMetalDiameter + 2*fromMils(3)
-
-for i in 0..7:
-  let x = fromMils((float(i) - 3.5) * 100)
-  let number = $(i + 1)
-  let name = "pin" & number
-  discard Pin(
-    centerPos: (x: x, y: fromMils(50)),
-    metalDiameter: pinMetalDiameter,
-    clearance: fromMils(30),
-    maskDiameter: pinMaskDiameter,
-    drillDiameter: pinDrillDiameter,
-    name: name,
-    number: number,
-    flags: {},
-  ).addTo(element)
-  discard Pin(
-    centerPos: (x: x, y: fromMils(-50)),
-    metalDiameter: pinMetalDiameter,
-    clearance: fromMils(30),
-    maskDiameter: pinMaskDiameter,
-    drillDiameter: pinDrillDiameter,
-    name: name,
-    number: number,
-    flags: {},
-  ).addTo(element)
-
-dump element
+const element = createElement()
+echo $element
--- eda/footprints/POWER_PADS.nim
+++ eda/footprints/POWER_PADS.nim
@@ -1,60 +1,63 @@
-import k12a.eda.footprintdsl
+import pcb.types
+import pcb.stringify
 
-let pinOffset = fromMils(75)
+proc createElement(): Element =
+  const pinOffset = 75.mil()
 
-let pinDrillDiameter = fromMils(28) # 22AWG hook-up wire is just over 0.6mm diameter
-let pinMetalDiameter = pinDrillDiameter + 2*fromMils(16)
-let pinMaskDiameter = pinMetalDiameter + 2*fromMils(3)
+  const pinDrillDiameter = 28.mil()
+  const pinMetalDiameter = pinDrillDiameter + 2 * 16.mil()
+  const pinMaskDiameter = pinMetalDiameter + 2 * 3.mil()
+  const silkThickness = 10.mil()
 
-var element = Element(
-  flags: {},
-  description: "Power connector pads",
-  markPosition: fromMils(200, 200),
-  namePosition: (x: -pinOffset, y: fromMils(50)),
-  nameRotation: 0,
-  nameScale: 100,
-  nameFlags: {},
-  children: @[],
-)
+  var element: Element
+  element.init()
+  element.desc = "POWER_PADS"
+  element.markPos = (x: 200.mil(), y: 200.mil())
+  element.textPos = (x: -pinOffset, y: 50.mil())
+  element.textDir = leftToRight
+  element.textScale = 100
+  
+  var vccPin: Pin
+  vccPin.init()
+  vccPin.center = (x: -pinOffset, y: 0.mil())
+  vccPin.metalDiameter = pinMetalDiameter
+  vccPin.clearance = 30.mil()
+  vccPin.maskDiameter = pinMaskDiameter
+  vccPin.drillDiameter = pinDrillDiameter
+  vccPin.name = "VCC"
+  vccPin.number = "1"
+  element.pins.add(vccPin)
+  
+  var gndPin: Pin
+  gndPin.init()
+  gndPin.center = (x: pinOffset, y: 0.mil())
+  gndPin.metalDiameter = pinMetalDiameter
+  gndPin.clearance = 30.mil()
+  gndPin.maskDiameter = pinMaskDiameter
+  gndPin.drillDiameter = pinDrillDiameter
+  gndPin.name = "GND"
+  gndPin.number = "2"
+  element.pins.add(gndPin)
+  
+  var line1: ElementLine
+  line1.point1 = (x: pinOffset - 20.mil(), y: -pinOffset)
+  line1.point2 = (x: pinOffset + 20.mil(), y: -pinOffset)
+  line1.thickness = silkThickness
+  element.lines.add(line1)
+  
+  var line2: ElementLine
+  line2.point1 = (x: -pinOffset - 20.mil(), y: -pinOffset)
+  line2.point2 = (x: -pinOffset + 20.mil(), y: -pinOffset)
+  line2.thickness = silkThickness
+  element.lines.add(line2)
+  
+  var line3: ElementLine
+  line3.point1 = (x: -pinOffset, y: -pinOffset - 20.mil())
+  line3.point2 = (x: -pinOffset, y: -pinOffset + 20.mil())
+  line3.thickness = silkThickness
+  element.lines.add(line3)
+  
+  result = element
 
-discard Pin(
-  centerPos: (x: -pinOffset, y: fromMils(0)),
-  metalDiameter: pinMetalDiameter,
-  clearance: fromMils(30),
-  maskDiameter: pinMaskDiameter,
-  drillDiameter: pinDrillDiameter,
-  name: "VCC",
-  number: "1",
-  flags: {},
-).addTo(element)
-
-discard Pin(
-  centerPos: (x: pinOffset, y: fromMils(0)),
-  metalDiameter: pinMetalDiameter,
-  clearance: fromMils(30),
-  maskDiameter: pinMaskDiameter,
-  drillDiameter: pinDrillDiameter,
-  name: "GND",
-  number: "2",
-  flags: {},
-).addTo(element)
-
-discard Line(
-  pos1: (x: pinOffset - fromMils(20), y: -pinOffset),
-  pos2: (x: pinOffset + fromMils(20), y: -pinOffset),
-  thickness: fromMils(10),
-).addTo(element)
-
-discard Line(
-  pos1: (x: -pinOffset - fromMils(20), y: -pinOffset),
-  pos2: (x: -pinOffset + fromMils(20), y: -pinOffset),
-  thickness: fromMils(10),
-).addTo(element)
-
-discard Line(
-  pos1: (x: -pinOffset, y: -pinOffset - fromMils(20)),
-  pos2: (x: -pinOffset, y: -pinOffset + fromMils(20)),
-  thickness: fromMils(10),
-).addTo(element)
-
-dump element
+const element = createElement()
+echo $element
